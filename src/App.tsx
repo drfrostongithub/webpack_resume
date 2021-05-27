76e8611ebf276e41adfb7021272530b8497975e3
drfrostongithub <eternal.putra@gmail.com>
2021-05-27 22:21:59 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const user = {
+  name: "Hayes",
+  id: "Dota",
+};
+interface User {
+  name: string;
+  id: number;
+}
+
+function App() {
+  return (
+    <div>
+      <h2>Welcome to React App</h2>
+      <h3>Date : {new Date().toDateString()}</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    // Where files should be sent once they are bundled
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    // webpack 5 comes with devserver whic loads in dev mode
+    devServer: {
+        port: 3000,
+        watchContentBase: true
+    },
+    // Rules of how webpack will take our files, complie & bundle them for the browser 
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /nodeModules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.(css|s[ac]ss)$/i,
+                use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })
+        , new MiniCssExtractPlugin()
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    }
+}
